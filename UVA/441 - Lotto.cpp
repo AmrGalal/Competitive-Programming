dcd2a8c61bf0fcd38d265dbc8a5001fdb48693e7
Amr Galal <amrheshamwahba@aucegypt.edu>
2017-06-10 16:05:24 +0200

    Create 441 - Lotto.cpp

--- UVA/441 - Lotto.cpp
+++ UVA/441 - Lotto.cpp
@@ -5,10 +5,13 @@ vector< int > values;
 int main()
 {
   ios_base::sync_with_stdio(false);
+  bool first = true;
   while(1)
   {
     int n; cin >> n;
     if(!n) break;
+    if(first) first = false;
+    else     printf("\n");
     values.clear();
     for(int i = 0; i < n; i++)
     {
@@ -21,7 +24,6 @@ int main()
     for(int fr5 = fr4+1;fr5 <= n-2; fr5++)
     for(int fr6 = fr5+1;fr6 <= n-1; fr6++)
     printf("%d %d %d %d %d %d\n",values[fr1],values[fr2],values[fr3],values[fr4],values[fr5],values[fr6]);
-    printf("\n");
   }
 
   return 0;
